a3b13cdd2b9f940e5e1a2f5e44d9ee20f7c2f23d
777Safal <tsafal06@gmail.com>
2023-09-07 10:58:38 +0545

    ne code

--- src/Page Component/Home/Home.jsx
+++ src/Page Component/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ball from '../../Image/5578341.jpg'
 
 
 
@@ -8,17 +9,16 @@ function Home() {
   ]
 
   return (
-    <div  className='w-full h-[100vh]'>    
-      <div className='w-10/12 h-2/4 flex items-end mx-auto'>
-        <h1 className='max-w-[22ch] uppercase text-4xl font-robotSlab font-bold tracking-wider text-zinc-50 '>{data[0].h1}</h1>
-      </div>
-<div className='w-10 h-10' style={{
-  backgroundImage:`ur('src/Image/ballhiting.jpg')`,
-  
-}}>
-</div>
-      <div className=' flex w-10/12 mx-auto'>
-      </div>
+    <div  className=' w-10/12 mx-auto h-[100vh] overflow-hidden flex justify-center items-end' 
+    style={{ 
+      backgroundImage: `url(${ball})`,
+      backgroundPosition:'center',
+      backgroundRepeat:'no-repeat',
+      backgroundSize:'cover',
+    }}>    
+      <div className='w-fit mx-auto mb-5'>
+        <h1 className='max-w-[22ch] uppercase text-4xl font-robotSlab font-bold tracking-wider text-indigo-950 '>{data[0].h1}</h1>
+      </div>  
     </div>
   )
 }
--- src/Page Component/Navbar/Nav.jsx
+++ src/Page Component/Navbar/Nav.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {motion} from 'framer-motion'
+import logo from '../../Image/monalisaNObg.png'
 
 function Nav() {
+
+    const [showNav,setShowNav] = useState(false)
+
+    useEffect(() => {
+      window.addEventListener('scroll',actioning)
+    
+      return () => {
+        window.removeEventListener('scroll',actioning)
+      }
+    }, []);
+
+    const actioning=()=>{
+        if(window.scrollY > 100){
+            setShowNav(true);
+        }
+        else{
+            setShowNav(false);
+        }
+    };
+    
+
     const data=[
         {h1:'HOME'},
         {h1:'BOOK A COURT'},
@@ -14,24 +37,32 @@ function Nav() {
         '#about',
         '#contact'
     ]
+
+
   return (
-    <div className='absolute top-0 left-0 right-0 w-10/12 h-16 mx-auto flex items-center justify-center'>
+    <div className={`${showNav? 'fixed top-0 left-0 right-0 z-50 w-10/12':'relative w-10/12'} mx-auto bg-yellow-300`}>
         <div className='w-full flex items-center justify-between text-sm font-bbbb font-normal'>
-            <div className='flex items-center'>
-                <div 
-                className='w-16 h-16 mr-2 rounded-ful bg-cover bg-center 
-                bg-[url(https://o.remove.bg/downloads/173db0fb-611e-4810-98d1-0793f3fc01e1/images-removebg-preview.png)]'>
+            <div className=' ml-1 flex items-center'>
+                <div className='w-16 h-16 mr-2 rounded-ful' style={{
+                    backgroundImage:`url(${logo})`,
+                    backgroundSize:'cover'
+                }}>
                 </div>
-                <div className='text-center text-lg font-bold text-red-600'>Monalisa Futsal</div>
+                <motion.h1 whileHover={{scale:1.3}}
+                className='text-center text-xl font-bold text-red-600'>Monalisa Futsal</motion.h1>
             </div>
             <ul className='flex gap-3 items-center'>
             {data.map((val,i)=>{
-                return <li key={i} className=''>
-                    <a href={href[i]} className='px-3 py-2 text-center rounded-lg hover:bg-red-600 hover:text-white'>{val.h1}</a>
+                return <li key={i} className='' >
+                    <a href={href[i]} className='px-3 py-2 hover:bg-red-600 hover:text-slate-50 text-center rounded-lg '>{val.h1}</a>
                 </li>
             })}
             </ul>
-            <div className='px-3 py-2 text-left rounded-lg hover:bg-red-600 hover:text-white cursor-pointer'>Login</div>
+            <motion.button 
+            whileHover={{scale:1.3}}
+            transition={{duration:0.1}}
+            
+            className='px-3 py-2 text-left rounded-lg bg-red-600 text-white cursor-pointer mr-3'>Login</motion.button>
         </div>
     </div>
   )
